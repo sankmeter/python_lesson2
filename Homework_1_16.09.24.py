c0c116d0803f0b01964d300a217721ebf7c2d0a9
sankmeter <danik67you@gmail.com>
2024-09-23 12:57:54 +0200

    Write 1234 or other numers

--- Homework_1_16.09.24.py
+++ Homework_1_16.09.24.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+f = int(input("введіть 4-значне число ")) #наприклад 1234
+print(f)
+number1 = f // 1000
+number2 = (f % 1000) //100
+number3 = (f % 100) //10
+number4 = f % 10
+print(number1)
+print(number2)
+print(number3)
+print(number4)
